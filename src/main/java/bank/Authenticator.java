878004bf0b46e8767b76fc8a5c45991ab8bc1c68
Rohan_Nagula07 <84406519+Rohan-Nagula@users.noreply.github.com>
2024-10-15 11:57:16 +0000

    "until login page done"

---
+++ src/main/java/bank/Authenticator.java
@@ -0,0 +1,19 @@
+package bank;
+
+import javax.security.auth.login.LoginException;
+
+public class Authenticator {
+  public static Customer login(String username, String password) throws LoginException{
+    Customer customer = DataSource.getCustomer(username);
+    if(customer == null){
+      throw new LoginException("Username not found");
+    }
+    if(password.equals(customer.getPassword()))
+
+  }
+
+  public static void logout(Customer customer) {
+
+  }
+
+}
